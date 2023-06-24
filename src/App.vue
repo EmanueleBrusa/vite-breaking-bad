f24e31fa8ef1d2fa32826b81c9b0cb8f33317976
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-06-24 09:59:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+// import axios from 'axios';
+import AppPokemonMain from './components/AppPokemonMain.vue';
+  export default {
+    components: {AppPokemonMain
+    }
+  }
+</script>
+<template>
+  <div>
+    <AppPokemonMain />
+  </div>
+</template>
+<style lang="scss">
+  
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import axios from 'axios';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
+createApp(App).mount('#app')
